5a05cd2a4a2ddeb4a2db527849dc5c81e14a0e92
Keir Havel <keirhavel@Keirs-MacBook-Pro.local>
2023-11-25 21:09:53 -0800

    adding hebbian conv

--- backprop_alts/hebbian_learning.py
+++ backprop_alts/hebbian_learning.py
@@ -1,5 +1,9 @@
 import torch
 import einops
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
+
 
 def hebbian_wta(x, y, W):
     """
@@ -56,6 +60,77 @@ def hebbian_pca(x, y, W):
 
     weight_update = (pre_post_correlation - weight_expectation) / batch_size
     return weight_update
+
+class HebbianConv2d(torch.nn.Module):
+    """
+    Hebbian convolutional layer. This layer is implemented as a torch.nn.Conv2d
+    layer with a custom weight update function. The weight update function is
+    the FastHebb formulation, which is a more numerically stable version of
+    the Hebbian learning rule.
+
+    Parameters
+    ----------
+    in_channels : int
+        Number of input channels
+    out_channels : int
+        Number of output channels
+    kernel_size : int
+        Size of the convolutional kernel
+    stride : int
+        Stride of the convolutional kernel
+    padding : int
+        Padding of the convolutional kernel
+    dilation : int
+        Dilation of the convolutional kernel
+    bias : bool
+        Whether or not to include a bias term
+    """
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride = 1,
+                 padding = 0):
+        super().__init__()
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size, kernel_size)
+        if isinstance(stride, int):
+            stride = (stride, stride)
+        if isinstance(padding, int):
+            padding = (padding, padding)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        self.weight = torch.randn(out_channels, in_channels, kernel_size[0], kernel_size[1])
+
+    def forward(self, x):
+        return torch.nn.functional.conv2d(x, self.weight, stride = self.stride, padding = self.padding)
+    
+    def train_step(self, x, lr = 0.01):
+        batch_size = x.shape[0]
+        x_unfolded = torch.nn.functional.unfold(x, 
+                                                kernel_size = self.kernel_size, 
+                                                stride = self.stride, 
+                                                padding = self.padding)
+        n_blocks = x_unfolded.shape[-1]
+        x_unfolded = einops.rearrange(x_unfolded, 
+                                      "...(c k1 k2) n -> ... c n k1 k2", 
+                                      c = self.in_channels, 
+                                      k1 = self.kernel_size[0], 
+                                      k2 = self.kernel_size[1])
+                                      
+        y = self.forward(x)
+        y_unfolded = einops.rearrange(y, "... c h w -> ... c (h w)")
+
+        # sum over batch and image chunks
+        dW = torch.einsum("bcnij, bkn -> kcij", x_unfolded, y_unfolded)
+        dW /= batch_size * n_blocks
+
+        self.weight -= lr * dW
+        return y
 
 def simple_test_plots(weights, pca = False, scale_max = 5, n_clusters = 3, centers = None, points = None):
     # plot weights and centers
@@ -156,10 +231,65 @@ def test_simple(n_iters = 1000,
 
         simple_test_plots(weights, pca = pca, scale_max = scale_max, n_clusters = n_clusters, centers = centers, points = points)
 
+def test_conv(n_epochs = 2,
+              kernel_size = 3,
+              stride = 2,
+              batch_size = 256,
+              mid_channels = 32,
+              out_channels = 64,
+              lr = 0.01,
+              plot = True):
+    """
+    Function that tests the Hebbian convolution, by plotting the filters.
+    """
+    from utils import _prepare_for_epochs
+    from torchvision.utils import make_grid
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    with torch.no_grad():
+        conv = torch.nn.Sequential(HebbianConv2d(1, mid_channels, kernel_size, stride = stride, padding = 1),
+                                   HebbianConv2d(mid_channels, out_channels, kernel_size, stride = stride, padding = 1)).to(device)
+        initial_weights = [x.weight.clone().detach().cpu() for x in conv]
+        mnist, mnist_val, accs, errors = _prepare_for_epochs()
+
+        for epoch in range(n_epochs):
+            train_loader = torch.utils.data.DataLoader(mnist, 
+                                                        batch_size = batch_size, 
+                                                        shuffle = True)
+            for i, (x, y) in tqdm(enumerate(train_loader)):
+                x = x.to(device)
+                x = x.view(-1, 1, 28, 28)
+                y = x.clone()
+                for layer in conv:
+                    y = layer.train_step(y, lr = lr)
+
+        weights = [x.weight.clone().detach().cpu() for x in conv]
+        if plot:
+            fig, ax = plt.subplots(1, 2, figsize = (14, 7))
+            weight, initial_weight = [], []
+
+            for i, (initial_weight_i, weight_i) in enumerate(zip(initial_weights, weights)):
+                if i > 0:
+                    initial_weight_i = initial_weight_i[0].unsqueeze(1)
+                    weight_i = weight_i[0].unsqueeze(1)
+                approx_sqrt = int(np.sqrt(initial_weight_i.shape[0]))
+                initial_weight_i = make_grid(initial_weight_i, 
+                                             nrow = approx_sqrt)
+                weight_i = make_grid(weight_i,
+                                    nrow = approx_sqrt)
+                initial_weight.append(initial_weight_i)
+                weight.append(weight_i)
+            initial_weight = torch.cat(initial_weight, dim = 1)
+            weight = torch.cat(weight, dim = 1)
+
+            ax[0].imshow(initial_weight.permute(1, 2, 0))
+            ax[1].imshow(weight.permute(1, 2, 0))
+            ax[0].set_title("Initial weights")
+            ax[1].set_title("Final weights")
+            plt.show()
+        return conv
 
 if __name__ == "__main__":
-    from tqdm import tqdm
-    import numpy as np
-    import matplotlib.pyplot as plt
 
-    test_simple(pca = False)
+    #test_simple(pca = False)
+    conv = test_conv()
--- backprop_alts/utils.py
+++ backprop_alts/utils.py
@@ -2,6 +2,7 @@ from tqdm import tqdm
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import gymnasium as gym
 from time import time
 from torchvision import datasets, transforms
 
@@ -78,3 +79,146 @@ def mnist_test(net,
             details["epoch_samples"].append(i * batch_size)
 
         return accs, errors, y, details
+    
+def atari_assault_test(net,
+                       env_name = "AssaultNoFrameskip-v4",
+                       n_epochs = 3,
+                       n_labels = 10,
+                       save_every = 10,
+                       device = torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+    
+    details = {"epoch_accs" : [],
+               "epoch_times" : [],
+               "epoch_samples" : [],}
+    
+    net.to(device)
+
+    with torch.no_grad():
+
+        env = gym.make(env_name)
+        env.reset()
+        accs = []
+        errors = []
+
+        for epoch in range(n_epochs):
+            epoch_accs = []
+            start = time()
+            for i in tqdm(range(1000)):
+                x = env.render(mode = "rgb_array")
+                x = torch.tensor(x, dtype = torch.float32)
+                x = x.permute(2, 0, 1)
+                x = x.unsqueeze(0)
+                x = x.to(device)
+
+                #TODO
+
+class ActorPerciever(torch.nn.Module):
+    def __init__(self,
+                 encoder_final_hw = 20,
+                 latent_dim = 64,
+                 encoder_depth = 5,
+                 actor_depth = 3,
+                 perciever_depth = 3,
+                 activation = torch.nn.GELU(),
+                 in_channels = 3,
+                 n_actions = 7,
+                 perciever_temp = 0.1,
+                 actor_temp = 0.4):
+        super().__init__()
+        self.latent_dim = latent_dim
+        self.encoder_depth = encoder_depth
+        self.encoder_final_hw = encoder_final_hw
+        self.actor_depth = actor_depth
+        self.perciever_depth = perciever_depth
+        self.activation = activation
+        self.softmax = torch.nn.Softmax(dim = -1)
+        self.in_channels = in_channels
+        self.n_actions = n_actions
+        self.perciever_temp = perciever_temp
+        self.actor_temp = actor_temp
+
+        mult_per_layer = int(np.exp(np.log(latent_dim / in_channels) / encoder_depth))
+        in_outs = [in_channels] + [in_channels * mult_per_layer ** i for i in range(1, encoder_depth)] + [latent_dim]
+
+        self.norm = torch.nn.LayerNorm(in_channels)
+        encoder = [torch.nn.Conv2d(in_outs[i], in_outs[i + 1], kernel_size = 3, stride = 2) for i in range(encoder_depth)]
+        self.final_encoder = torch.nn.Linear(encoder_final_hw, 1)
+        self.encoder = torch.nn.ModuleList(encoder)
+
+        actor = [torch.nn.Linear(latent_dim, latent_dim) for i in range(actor_depth - 1)]
+        self.final_act = torch.nn.Linear(latent_dim, n_actions)
+        self.actor = torch.nn.ModuleList(actor)
+
+        perciever = [torch.nn.Linear(latent_dim, latent_dim) for i in range(perciever_depth)]
+        self.perciever = torch.nn.ModuleList(perciever)
+
+        self.efference_table = torch.nn.Embedding(n_actions, latent_dim)
+
+    def encode(self, x):
+        x = self.norm(x).permute(0, 3, 1, 2)
+        for layer in self.encoder:
+            x = layer(x)
+            x = self.activation(x)
+        x = x.view(-1, self.latent_dim, self.encoder_final_hw)
+        x = self.final_encoder(x).squeeze(-1)
+        return self.softmax(x)
+    
+    def act(self, x):
+        for layer in self.actor:
+            x = layer(x)
+            x = self.activation(x)
+        act = self.final_act(x)
+        act = self.softmax(act / self.actor_temp)
+        return act
+    
+    def perceive(self, x, efference = None):
+        if efference is not None:
+            act = torch.argmax(efference, dim = -1)
+            x = x + self.efference_table(act)
+        for layer in self.perciever:
+            x = layer(x)
+            x = self.activation(x)
+        x = self.softmax(x / self.perciever_temp)
+        return x
+    
+    def forward(self, x):
+        encoding = self.encode(x)
+        action = self.act(encoding)
+        perception = self.perceive(encoding, efference = action)
+        return action, perception
+    
+    def train_step(self, x_tminus, env, optimizer_p, optimizer_a):
+        optimizer_p.zero_grad()
+        optimizer_a.zero_grad()
+
+        action, perception = self(x_tminus)
+
+        xtplus, reward, done, _, _ = env.step(torch.argmax(action, dim = -1))
+        xtplus = torch.tensor(xtplus, dtype = torch.float32)
+        perception_plus = self.encode(xtplus.unsqueeze(0))
+
+        loss_p = torch.nn.functional.cross_entropy(perception, perception_plus)
+        loss_a = -loss_p
+
+        loss_p.backward(retain_graph = True)
+        optimizer_p.step()
+
+        loss_a.backward()
+        optimizer_a.step()
+        return xtplus, loss_p.item()
+
+
+if __name__ == "__main__":
+    from itertools import chain
+    ap = ActorPerciever()
+    optimizer_p = torch.optim.Adam(chain(ap.encoder.parameters(), ap.perciever.parameters()), lr = 0.001)
+    optimizer_a = torch.optim.Adam(chain(ap.encoder.parameters(), ap.actor.parameters()), lr = 0.001)
+
+    env = gym.make("AssaultNoFrameskip-v4")
+    x, start_md = env.reset()
+    x = torch.Tensor(x).unsqueeze(0)
+    losses = []
+    for i in tqdm(range(1000)):
+        x, loss = ap.train_step(x, env, optimizer_p, optimizer_a)
+        losses.append(loss)
+    plt.plot(losses)
